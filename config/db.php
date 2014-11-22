c76b268df35cd22d7a5655e054c1f33fca31f80b
Jain <jain@ubuntu.ubuntu-domain>
2014-11-22 23:03:13 +0530

    first commit of hello world for yii2

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=yii2',
+    'username' => 'root',
+    'password' => 'sajain',
+    'charset' => 'utf8',
+];
